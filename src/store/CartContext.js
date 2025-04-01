7f6a8bb6fad010ded33944e99474b35dae33a81c
Student <student@Administrators-Mac-mini.local>
2025-04-01 16:46:46 +0300

    yl8: mitmekordne lisamine suurendab kogust

--- src/store/CartContext.js
+++ src/store/CartContext.js
@@ -1,19 +1,45 @@
-import { createContext, useState } from 'react';
+import { createContext, useReducer } from 'react';
 
 const CartContext = createContext({
     items: [],
     addItem: (item) => {},
 });
 
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD') {
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.item.id
+        );
+        const updatedItems = [...state.items];
+        if (existingCartItemIndex !== -1) {
+            updatedItems[existingCartItemIndex] = {
+                ...updatedItems[existingCartItemIndex],
+                quantity: updatedItems[existingCartItemIndex].quantity + 1,
+            };
+        } else {
+            updatedItems.push({ ...action.item, quantity: 1 });
+        }
+
+        return {
+            ...state,
+            items: updatedItems,
+        };
+    }
+    return state;
+};
+
+
+
+
 export const CartContextProvider = (props) => {
-    const [cartItems, setCartItems] = useState([]);
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, { items: [] });
 
     const addItemHandler = (item) => {
-        setCartItems((prevCartItems) => [...prevCartItems, item]);
+        dispatchCartAction({ type: 'ADD', item });
     };
 
     const contextValue = {
-        items: cartItems,
+        items: cartState.items,
         addItem: addItemHandler,
     };
 
