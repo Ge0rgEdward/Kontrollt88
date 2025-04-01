08c0a92a2b10938882c834371594842bdec9016e
Student <student@Administrators-Mac-mini.local>
2025-04-01 13:30:39 +0300

    yl3 andmete kuvamine konsoolis

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,11 +1,31 @@
+import { useEffect, useState } from 'react';
+
 const Meals = () => {
+    const [meals, setMeals] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:3001/meals")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setMeals(data);
+            })
+    }, []);
     return (
         <ul id="meals">
-            { 
-                // list of meals
+            { meals.map((meal) => (
+                <li key={meal.id}>
+                    <h3>{meal.name}</h3>
+                    <p>{meal.description}</p>
+                    <p>{meal.price}</p>
+                </li>
+            ))
             }
         </ul>
-    )
-}
+
+        
+    );
+
+    console.log(meals);
+};
 
 export default Meals
