b9f640333416e699b5f0aa5f78d4df82f6313d1a
Student <student@Administrators-Mac-mini.local>
2025-04-01 14:33:45 +0300

    yl5 uus hinna fromaat

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,11 +1,17 @@
 const MealItem = (props) => {
+
+        const formattedPirce = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'EUR'
+        }).format(props.meal.price);
+
     return (
         <li className="meal-item">
             <article>
                 <img src={require(`../assets/${props.meal.image}`)} alt={props.meal.name}/>
                 <div className = "meal-item-description">
                     <h3>{props.meal.name}</h3>
-                    <p classname="meal-item-price">{props.meal.price}</p>
+                    <p classname="meal-item-price">{formattedPirce}</p>
                     <p>{props.meal.description}</p>
                 </div>
                 <p className="meal-item-actions">
